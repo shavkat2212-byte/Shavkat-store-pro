76163f6a4c6eb8ec2c50c85e80e74c8a58efd28c
shavkat2212-byte <shavkat2212@gmail.com>
2026-06-28 07:27:45 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Shavkat Store Pro",
+    page_icon="🏪",
+    layout="wide"
+)
+
+st.title("🏪 Shavkat Store Pro")
+
+st.success("Система успешно запущена!")
+
+st.write("Добро пожаловать в новую версию программы учета магазина.")
