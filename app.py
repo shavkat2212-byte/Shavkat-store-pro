8323ce9a5754bf9ba1bb9885605189066014a83a
shavkat2212-byte <shavkat2212@gmail.com>
2026-06-29 07:39:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from database import supabase
 
 st.set_page_config(
     page_title="Shavkat Store Pro",
@@ -7,19 +6,12 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("🏪 Shavkat Store Pro")
+st.title("Shavkat Store Pro")
 
-st.write("## Проверка подключения к Supabase")
+st.write("Если ты видишь это сообщение, значит app.py работает.")
 
 try:
-    result = supabase.table("products").select("*").limit(1).execute()
-
-    st.success("✅ Подключение к Supabase успешно!")
-
-    st.write("База данных отвечает.")
-
+    import pages
+    st.success("Папка pages найдена")
 except Exception as e:
-
-    st.error("❌ Не удалось подключиться к Supabase")
-
-    st.code(str(e))
+    st.error(e)
